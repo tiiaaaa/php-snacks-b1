4b221e739ca8153c1911666fcc06e2434c6e88ed
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-20 17:56:48 +0200

    completati snacks 5 e 6

--- snak5/index.php
+++ snak5/index.php
@@ -3,10 +3,10 @@
 Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.*/
 
 $paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos expedita, esse magni deserunt maiores doloremque aliquam officiis ipsa. Qui est animi, quae non atque dicta culpa dolorum laudantium eum!
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia atque sint ullam explicabo? Fugiat et, inventore quia quae illum asperiores! Asperiores reiciendis hic, repudiandae incidunt tenetur possimus fugit sequi laboriosam.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate error atque aut quia, iste, fugiat exercitationem beatae iusto et nobis assumenda odit, accusantium ab itaque consequatur iure nesciunt minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quos officia at reiciendis vitae! Repellendus culpa voluptate ullam rem, quia magnam impedit mollitia? Ipsam facere id ipsum vero quaerat assumenda. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quod nesciunt fuga eaque consequuntur, distinctio ipsa iure asperiores exercitationem pariatur molestias porro optio ipsum repudiandae necessitatibus corrupti quos hic nam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores inventore dolor ipsa expedita eius, dolorum ipsum saepe, veritatis cupiditate consequatur numquam blanditiis cumque nobis possimus odio sit repellendus, officiis alias. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus nulla amet eos nemo repudiandae natus aut sequi voluptatibus veritatis aspernatur voluptatum aliquid officiis incidunt totam ex, asperiores numquam unde voluptatem.';
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia atque sint ullam explicabo? Fugiat et, inventore quia quae illum asperiores! Asperiores reiciendis hic, repudiandae incidunt tenetur possimus fugit sequi laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate error atque aut quia, iste, fugiat exercitationem beatae iusto et nobis assumenda odit, accusantium ab itaque consequatur iure nesciunt minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quos officia at reiciendis vitae! Repellendus culpa voluptate ullam rem, quia magnam impedit mollitia? Ipsam facere id ipsum vero quaerat assumenda. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quod nesciunt fuga eaque consequuntur, distinctio ipsa iure asperiores exercitationem pariatur molestias porro optio ipsum repudiandae necessitatibus corrupti quos hic nam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores inventore dolor ipsa expedita eius, dolorum ipsum saepe, veritatis cupiditate consequatur numquam blanditiis cumque nobis possimus odio sit repellendus, officiis alias. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus nulla amet eos nemo repudiandae natus aut sequi voluptatibus veritatis aspernatur voluptatum aliquid officiis incidunt totam ex, asperiores numquam unde voluptatem';
 
-
+/* Utilizzo la funzione 'explode' per dividere la stringa pricipale ($patragraph) in tante sotto stringhe quanti sono i punti (.)*/
+$subStrings = explode('.', $paragraph);
 
 ?>
 
@@ -19,8 +19,12 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate err
     <title>Snak 5</title>
 </head>
 <body>
+    <!-- Ciclo per la lunghezza di $subStrings e stampo ogni volta una stringa all' indice 'i' -->
+    <?php for($i = 0; $i < count($subStrings); $i++) { ?>
     <p>
-
+        <?php echo $subStrings[$i]. ' . '; ?>
     </p>
+
+    <?php } ?>
 </body>
 </html>
--- snak6/index.php
+++ snak6/index.php
@@ -24,6 +24,13 @@ $db = [
         ]
     ]
 ];
+
+/* Dall' array princibale DB estrapolo i le due chiavi 'teachers' e 'pm' che a loro volta 
+sono Array e le salvo in due variabili */
+$teachers = $db['teachers'];
+$pm = $db['pm'];
+
+
 ?>
 
 
@@ -38,13 +45,27 @@ $db = [
 </head>
 <body>
     <h2>teachers</h2>
-    <div class="grey">
-
+    <div class="grey"> 
+        <ul>
+            <!-- Ciclo poi per la lunghezza della variabile teachers e stampo il valore 'name' e 'lastname' all'indice 'i'-->
+            <?php for($i = 0; $i < count($teachers); $i++) { ?>
+                <li>
+                    <?php echo $teachers[$i]['name'] . ' ' . $teachers[$i]['lastname']; ?>
+                </li>
+            <?php } ?>
+        </ul>
     </div>
     
     <h2>pm</h2>
     <div class="green">
-
+        <ul>
+            <!-- Ciclo poi per la lunghezza della variabile pm e stampo il valore 'name' e 'lastname' all'indice 'i'-->
+            <?php for($i = 0; $i < count($pm); $i++) { ?>
+                <li>
+                    <?php echo $pm[$i]['name'] . ' ' . $pm[$i]['lastname']; ?>
+                </li>
+            <?php } ?>
+        </ul>
     </div>
     
 </body>
